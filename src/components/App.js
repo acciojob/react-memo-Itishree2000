e61e4df91d08513b7b124ab2216e0e97ace8e777
Itishree Jena <itishreej203@gmail.com>
2023-12-19 20:48:33 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,58 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useState} from "react";
 
+const App = () => {
+  const [todos, setToDo] = useState([]);
+  const [inputText, setInputText] = useState("");
+  const [count, setCount] = useState(0);
+  const [calculationResult, setCalculationResult] = useState("");
+  function handleTodo() {
+    setToDo([...todos, { id: todos.length + 1, content: "New todo" }]);
+  }
+  function handleInputValue(event) {
+    setInputText(event.target.value);
+  }
+  function handleCount() {
+    setCount((prevCount) => prevCount + 1);
+    const result=expensiveCalculation();
+    setCalculationResult(result);
+  }
+  function expensiveCalculation(){
+    const result=count*10000000;
+    return result;
+  }
+  
+  function handleAddItem() {
+    if (inputText.length > 5) {
+      setToDo([...todos, { id: todos.length + 1, content: inputText }]);
+      setInputText("");
+    } else {
+      alert("Text should be greater than 5 characters.");
+    }
+  }
+
+  return (
+    <div>
+      <h1>React.useMemo</h1>
+      <h1>My todos</h1>
+      <div>
+        <button onClick={handleTodo}>Add Todo</button>
+        <button onClick={handleCount}>Count:{count} +</button>
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder=""
+          value={inputText}
+          onChange={handleInputValue}
+        />
+        <button onClick={handleAddItem}>Add Skill</button>
+      </div>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.content}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+export default App;
